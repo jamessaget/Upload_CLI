685036844eefd36cb4012ff5c64b25517846abc1
jsaget <james@fanatic.co.uk>
2021-07-06 08:02:49 +0100

    refactor factory to pass different storagedriver

--- src/Factory/CliFactory.php
+++ src/Factory/CliFactory.php
@@ -1,22 +1,10 @@
 <?php 
 namespace App\Factory;
 use App\Classes\ImageStorageCli;
-use Monolog\Logger;
-use Monolog\Handler\StreamHandler;
-use App\Classes\LocalStorageDriver;
-use App\Config\ConfigOptions;
+use App\Interfaces\StorageDriver;
 
 class CliFactory {
-    public static function create(string $option){
-
-        // Setup Logger
-        $log = new Logger('CLI Log');
-        $log->pushHandler(new StreamHandler(__DIR__.'/debug.log', Logger::DEBUG));
-        new ConfigOptions($log);
-
-        // Setup LocalStorageDriver Class 
-        $storage_driver = new LocalStorageDriver($log);
-
+    public static function create(StorageDriver $storage_driver, string $option){
         return new ImageStorageCLI($storage_driver, $option);
     }
 }
