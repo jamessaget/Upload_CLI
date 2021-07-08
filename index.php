0f6ae30d638bfd8b252c566927080a1b36e9412b
jsaget <james@fanatic.co.uk>
2021-07-08 08:07:22 +0100

    refactoring factory based of driver type

--- index.php
+++ index.php
@@ -2,10 +2,6 @@
 require './vendor/autoload.php';
 
 use App\Factory\CliFactory;
-use Monolog\Logger;
-use Monolog\Handler\StreamHandler;
-use App\Classes\LocalStorageDriver;
-use App\Config\ConfigOptions;
 
 // Define & get flags passed to console
 $option = getopt('', [ 
@@ -18,14 +14,6 @@ if(count($option) == 1){
     // Option from commandline
     $option = array_keys($option);
 
-    // Setup Logger
-    $log = new Logger('CLI Log');
-    $log->pushHandler(new StreamHandler(__DIR__ . '/debug.log', Logger::DEBUG));
-    new ConfigOptions($log);
-
-    // Setup LocalStorageDriver Class 
-    $storage_driver = new LocalStorageDriver($log);
-
     // Initialize CLI
-    $storage_cli = CliFactory::create($storage_driver, $option[0]);
+    $storage_cli = CliFactory::create('local', $option[0]);
 }
--- src/Factory/CliFactory.php
+++ src/Factory/CliFactory.php
@@ -1,10 +1,31 @@
 <?php 
 namespace App\Factory;
 use App\Classes\ImageStorageCli;
-use App\Interfaces\StorageDriver;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+use App\Classes\LocalStorageDriver;
+use App\Config\ConfigOptions;
+use ErrorException;
 
 class CliFactory {
-    public static function create(StorageDriver $storage_driver, string $option){
+    public static function create(string $storage_driver, string $option){
+
+        // Setup Logger
+        $log = new Logger('CLI Log');
+        $log->pushHandler(new StreamHandler(__DIR__ . '/debug.log', Logger::DEBUG));
+        new ConfigOptions($log);
+
+        try {
+            switch($storage_driver) {
+                case 'local': 
+                    $storage_driver = new LocalStorageDriver($log);
+                default:
+                    throw new ErrorException('Storage driver not found');
+            }    
+        } catch(\Exception $ex){
+            echo $ex->getMessage();
+        }
+
         return new ImageStorageCLI($storage_driver, $option);
     }
 }
